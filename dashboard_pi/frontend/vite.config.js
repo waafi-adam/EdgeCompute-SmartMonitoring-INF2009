b02d64f259796cec5f4e89cbcc67e59a290a4dea
Waafi Adam <abdullah.waafi@gmail.com>
2025-03-11 15:06:47 +0800

    vite allow all network

--- dashboard_pi/frontend/vite.config.js
+++ dashboard_pi/frontend/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import { BACKEND_BASE_URL } from './src/config';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,4 +9,10 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  server: {
+    host: true, // Allow access from any network
+    strictPort: true, // Ensure Vite doesn't pick random ports
+    port: 5173, // Default Vite port, change if needed
+    allowedHosts: [`${BACKEND_BASE_URL}`], // Allow this hostname
+  },
 });
