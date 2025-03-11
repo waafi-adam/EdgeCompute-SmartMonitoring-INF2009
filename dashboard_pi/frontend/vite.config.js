3587d945468bb37c25727a3cd6e0670065f68d22
Waafi Adam <abdullah.waafi@gmail.com>
2025-03-11 15:07:58 +0800

    edit vite conf

--- dashboard_pi/frontend/vite.config.js
+++ dashboard_pi/frontend/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-import { BACKEND_BASE_URL } from './src/config';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,6 +12,5 @@ export default defineConfig({
     host: true, // Allow access from any network
     strictPort: true, // Ensure Vite doesn't pick random ports
     port: 5173, // Default Vite port, change if needed
-    allowedHosts: [`${BACKEND_BASE_URL}`], // Allow this hostname
   },
 });
